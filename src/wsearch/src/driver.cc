a78c9b35014205d912a20cc9317848de77e91799
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2019-11-08 23:38:52 -0800

    Setup the CMake infrastructure needed to build the code.
    Add in the CMakeLists.txt files needed to build the project.
    Using CMake may seem like overkill, but it will simplify
    our lives when we later begin unit testing with GoogleTest. This
    commit includes a 'hello world' program to test out the build
    process.

---
+++ src/wsearch/src/driver.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "grid_loader.h"
+
+int main(int argc, char** argv)
+{
+    do_nothing();
+    std::cout << "Hello, world." << std::endl;
+    return 0;
+}
---
+++ src/wsearch/src/grid_loader.cc
@@ -0,0 +1,7 @@
+#include "grid_loader.h"
+#include <iostream>
+
+void do_nothing()
+{
+    std::cout << "Gridloader talking..." << std::endl;
+}
